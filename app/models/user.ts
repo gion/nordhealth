be85288b0bf06e91d74cfed8c41fcb586970f4db
douablobs <doua.blobs@gmail.com>
2025-04-06 01:43:51 +0300

    chore: enhance zod error rules

--- app/models/user.ts
+++ app/models/user.ts
@@ -2,13 +2,15 @@ import { z } from 'zod'
 
 export const userSchema = z.object({
   name: z.string().min(2, 'Name must be at least 2 characters').optional().or(z.literal('')),
-  email: z
-    .string()
-    .min(1, 'An email address is required')
-    .email('Please enter a valid email address'),
+  email: z.string().email('Please enter a valid email address'),
   password: z
     .string()
-    .min(1, 'A password is required')
+    .regex(/^(?=.*[A-Z]).*$/, {
+      message: 'Password should contain at least one uppercase letter',
+    })
+    .regex(/^(?=.*\d.*)/, {
+      message: 'Password should contain at least one number',
+    })
     .min(8, 'Password must be at least 8 characters'),
   acceptMarketing: z.boolean(),
 })
